0e8aa4bacb738180c2d4906014aab90a53f3c92b
Emperor Starfinder <emperor@secondgalaxy.com>
2017-07-31 17:02:35 -0400

    Update ubuntu-installer.sh
    - Cleans up the installer script
    - Updates the installation commands with the -y at the end so it wont ask if you want to intall
    - Adds the command to secure the mysql database server after installation

--- Installers/Debian and Ubuntu/ubuntu-installer.sh
+++ Installers/Debian and Ubuntu/ubuntu-installer.sh
@@ -2,30 +2,23 @@
 # Virtual Universe on both debian and Ubuntu
 # The Idea came from WhiteCore-Sim which we give many thanks
 # Emperor Starfinder <emperor@secondgalaxy.com>
-# July 9, 2016
+# July 31, 2017
 
-# Install Mono 4.4
+# Install Mono 4.4 and above
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
-# Install mono-develop for bare essentials of mono
-sudo apt-get install mono-devel
-sudo apt-get update
 # Install mono-complete for the entire set of required mono tools
-sudo apt-get install mono-complete
-sudo apt-get update
-# Install mono reference assemblies to help suppress unnecessary errors with 
-# reference assemblies during building
-sudo apt-get install referenceassemblies-pcl
-sudo apt-get update
-#Install mono certificates
-sudo apt-get install ca-certificates-mono
+sudo apt-get install mono-complete -y
 sudo apt-get update
 # Install MySQL Server
-sudo apt-get install mysql-server
+sudo apt-get install mysql-server -y
+sudo apt-get update
+# Secure Database Server
+sudo mysql_secure_installation
 sudo apt-get update
 # Install Git
-sudo apt-get install git-core
+sudo apt-get install git-core -y
 sudo apt-get update
 # Setting up Virtual Universe
 cd /
